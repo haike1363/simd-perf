d4c2c9717e133f68c2c2c8ff4cacf21fb3b6421a
chenyi03 <chenyi03@corp.netease.com>
2018-10-23 08:19:56 +0800

    init

---
+++ simd/gorilla_avx.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int gorilla_avx_version() {
+    return 0;
+}
+
+int gorilla_avx_compress(double const * input, int input_size, char* ouput, int max_len) {
+    return 0;
+}
